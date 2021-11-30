4e8303308976c3bed538e1bf4b5a6877dca56753
zhoudan1 <zhoudan1@sensetime.com>
2021-11-30 19:51:36 +0800

    darw markview

--- lib/candlestick.dart
+++ lib/candlestick.dart
@@ -11,9 +11,20 @@ import 'package:mp_chart/mp/core/data/candle_data.dart';
 import 'package:mp_chart/mp/core/data_set/candle_data_set.dart';
 import 'package:mp_chart/mp/core/description.dart';
 import 'package:mp_chart/mp/core/entry/candle_entry.dart';
+import 'package:mp_chart/mp/core/entry/entry.dart';
 import 'package:mp_chart/mp/core/enums/axis_dependency.dart';
 import 'package:mp_chart/mp/core/enums/x_axis_position.dart';
+import 'package:mp_chart/mp/core/highlight/highlight.dart';
+import 'package:mp_chart/mp/core/marker/bar_chart_marker.dart';
+import 'package:mp_chart/mp/core/marker/horizontal_bar_chart_marker.dart';
+import 'package:mp_chart/mp/core/marker/i_marker.dart';
+import 'package:mp_chart/mp/core/marker/line_chart_marker.dart';
+import 'package:mp_chart/mp/core/marker/radar_chart_marker.dart';
+import 'package:mp_chart/mp/core/poolable/point.dart';
 import 'package:mp_chart/mp/core/utils/color_utils.dart';
+import 'package:mp_chart/mp/core/utils/painter_utils.dart';
+import 'package:mp_chart/mp/core/utils/utils.dart';
+import 'package:mp_chart/mp/core/value_formatter/default_value_formatter.dart';
 import 'package:mp_chart/mp/core/value_formatter/value_formatter.dart';
 import 'package:mp_chart/mp/core/value_formatter/my_value_formatter.dart';
 import 'action_state.dart';
@@ -48,8 +59,8 @@ class OtherChartCandlestickState
         Positioned(
             right: 0,
             left: 0,
-            top: 0,
-            bottom: 450,
+            top: 50,
+            bottom: 550,
             child: CandlestickChart(controller)),
       ],
     );
@@ -64,7 +75,7 @@ class OtherChartCandlestickState
             ..setAxisMaximum(42.0)
             ..setAxisMinimum(32.0)
             ..setGranularity(2.0)
-            ..drawGridLines = (false)  //Y轴的线条
+            ..drawGridLines = (true)  //Y轴的线条
             ..enableGridDashedLine(10, 10, 0) //Y轴虚线
             ..mDrawFilled=(true)
             ..setFilledTopY(38.0)
@@ -91,16 +102,21 @@ class OtherChartCandlestickState
             ..position = (XAxisPosition.BOTTOM);
 
         },
+        marker: MyChartMarker(),
         drawGridBackground: false,
         backgroundColor: ColorUtils.WHITE,
-        dragXEnabled: true,
-        dragYEnabled: true,
-        scaleXEnabled: true,
-        scaleYEnabled: true,
-        pinchZoomEnabled: true,
+        dragXEnabled: false,
+        dragYEnabled: false,
+        scaleXEnabled: false,
+        scaleYEnabled: false,
+        pinchZoomEnabled: false,
+        drawMarkers:true,
         maxVisibleCount: 60,
         description: desc);
   }
+
+  @override
+  IMarker initMarker() => BarChartMarker();
 
   void _initCandleData(int count, double range) async {
 //    chart.resetTracking();
@@ -215,5 +231,96 @@ class A extends ValueFormatter {
     } else {
       return _format.format(value);
     }
+  }
+}
+
+class MyChartMarker implements IMarker {
+  Entry _entry;
+  // ignore: unused_field
+  Highlight _highlight;
+  double _dx = 0.0;
+  double _dy = 0.0;
+
+  DefaultValueFormatter _formatter;
+  Color _textColor;
+  Color _backColor;
+  double _fontSize;
+
+  MyChartMarker({Color textColor, Color backColor, double fontSize})
+      : _textColor = textColor,
+        _backColor = backColor,
+        _fontSize = fontSize {
+    _formatter = DefaultValueFormatter(0);
+    this._textColor ??= ColorUtils.PURPLE;
+    this._backColor ??= ColorUtils.GRAY;
+    this._fontSize ??= Utils.convertDpToPixel(15);
+  }
+
+  @override
+  void draw(Canvas canvas, double posX, double posY) {
+    if(_entry is CandleEntry) {
+      double openvalue = (_entry as CandleEntry).open;
+      double closevalue = (_entry as CandleEntry).close;
+      int index = _entry.x.toInt();
+      String timeStrin = "";
+      if(index % 2 == 0) {
+        timeStrin = (index/2).truncate().toString() + ":00" + " ~ "  +  (index/2).truncate().toString() + ":30";
+      }else{
+        timeStrin = (index/2).truncate().toString() + ":30" + " ~ "  +  ((index + 1)/2).truncate().toString() + ":00";
+      }
+
+      TextPainter painter = PainterUtils.create(
+          null,
+          "$timeStrin\n${_formatter.getFormattedValue1(openvalue)}-${_formatter
+              .getFormattedValue1(closevalue)}",
+          _textColor,
+          _fontSize);
+      Paint paint = Paint()
+        ..color = _backColor
+        ..strokeWidth = 2
+        ..isAntiAlias = true
+        ..style = PaintingStyle.fill;
+
+      MPPointF offset = getOffsetForDrawingAtPoint(posX, posY);
+
+
+      canvas.save();
+      // translate to the correct position and draw
+//    canvas.translate(posX + offset.x, posY + offset.y);
+      painter.layout();
+      Offset pos = calculatePos(
+          posX + offset.x, posY + offset.y, painter.width, painter.height);
+
+      canvas.drawRRect(
+          RRect.fromLTRBR(pos.dx - 5, pos.dy - 5, pos.dx + painter.width + 5,
+              pos.dy + painter.height + 5, Radius.circular(5)),
+          paint);
+      painter.paint(canvas, pos);
+      canvas.restore();
+    }
+  }
+
+  Offset calculatePos(double posX, double posY, double textW, double textH) {
+    return Offset(posX - textW / 2, -textH/2);
+  }
+
+  @override
+  MPPointF getOffset() {
+    return MPPointF.getInstance1(_dx, _dy);
+  }
+
+  @override
+  MPPointF getOffsetForDrawingAtPoint(double posX, double posY) {
+    return getOffset();
+  }
+
+  @override
+  void refreshContent(Entry e, Highlight highlight) {
+    _entry = e;
+    highlight = highlight;
+  }
+
+  String format(double n) {
+    return n.toStringAsFixed(n.truncateToDouble() == n ? 0 : 2);
   }
 }
